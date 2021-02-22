b5ccbf2238b0413ef5f2ec27bb1c5ba6a926cb0c
Nelson Chaves <nelsonchavespro@gmail.com>
2021-02-21 18:15:38 -0800

    add roleable

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,24 +6,26 @@ class User < ApplicationRecord
          :confirmable, :trackable, :lockable,
          :omniauthable
 
-   def self.from_omniauth(access_token)
-     data = access_token.info
-     user = User.where(email: data['email']).first
-     unless user
-       user = User.create(
-         email: data['email'],
-         password: Devise.friendly_token[0,20]
-       )
-     end
-     user.provider = access_token.provider
-     user.uid = access_token.uid
-     unless user.name.present?
-       user.name = access_token.info.name
-     end
-     user.image = access_token.info.image
-     user.save
+  include Roleable
 
-     user.confirmed_at = Time.now # autoconfirm user from omniauth
-     user
-   end
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+    unless user
+      user = User.create(
+        email: data['email'],
+        password: Devise.friendly_token[0,20]
+      )
+    end
+    user.provider = access_token.provider
+    user.uid = access_token.uid
+    unless user.name.present?
+      user.name = access_token.info.name
+    end
+    user.image = access_token.info.image
+    user.save
+
+    user.confirmed_at = Time.now # autoconfirm user from omniauth
+    user
+  end
 end
